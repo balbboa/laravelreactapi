2717cb3f8a7e6f34411e35b3d6219b4a659d82c1
ARTHUR BALBOA DE MEDEIROS MARTINS <10871262401@pm.govrn>
2022-04-07 12:39:05 -0300

    Feat: implementação do registro e login, falta logout

--- routes/api.php
+++ routes/api.php
@@ -14,8 +14,9 @@ use App\Http\Controllers\API\AuthController;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
 Route::post('register', [AuthController::class, 'register']);
+Route::post('login', [AuthController::class, 'login']);
+
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
